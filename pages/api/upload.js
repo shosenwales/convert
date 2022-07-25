56cbe0d6b6d27ecc15a497fa768d09e99f8d9ce3
Shuaib Oseni <wales149@gmail.com>
2022-07-25 16:13:33 +0100

    fixes

--- pages/api/upload.js
+++ pages/api/upload.js
@@ -4,34 +4,45 @@ const cloudinary = require('cloudinary').v2;
 
 let storage = multer.memoryStorage();
 let upload = multer({
-    storage
+  storage,
 });
 
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
-  });
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET,
+});
 
-export default function handler(req, res){
-    upload.single('image')(req, {}, async (err) =>{
-        streamifier.createReadStream(req.file.buffer).pipe(
-            cloudinary.uploader.upload_stream({
-                folder: req.body.folder,
-                resource_type: req.body.type || "auto",
-                public_id: req.body.public_id 
-            }),
-            function(error, result){
-                if(error)res.status(500).send(error);
-                res.status(200).send(result);
-                res.end()
-            }
-        )
-    })
+export default async function handler(req, res) {
+  upload.single('image')(req, {}, async (err) => {
+    let result = await uploadFromBuffer(req);
+    const convertedImage = cloudinary.image(`${result.public_id}.jpg`);
+
+    res.send({ convertedImage });
+  });
+}
+
+let uploadFromBuffer = (req) => {
+  return new Promise((resolve, reject) => {
+    let cld_upload_stream = cloudinary.uploader.upload_stream(
+      {
+        folder: 'converted-images',
+      },
+      (error, result) => {
+        if (result) {
+          resolve(result);
+        } else {
+          reject(error);
+        }
+      }
+    );
+
+    streamifier.createReadStream(req.file.buffer).pipe(cld_upload_stream);
+  });
 };
 
-export const config ={
-    api:{
-        bodyParser: false
-    }
-} 
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
